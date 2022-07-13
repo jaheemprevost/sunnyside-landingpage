4ef2db0789146cb3670651d240d9f68018897121
Jaheem Prevost <jaheemdp@gmail.com>
2022-07-13 19:33:29 -0400

    Updated Files
    Updated html to include attribution in footer and readme file.

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
+//Grabs the DOM nodes corresponding with the mobile menu and its button.
 const menuBtn = document.querySelector('.menu-btn');
 const mobileMenu = document.querySelector('.mobile-menu');
 
+/*Function expression that will toggle the class hidden onto
+the mobile menu when called.*/
 const openMenu = function(){
     mobileMenu.classList.toggle("hidden");
 };
 
+//Attaches event listener that  executes the openMenu function.
 menuBtn.addEventListener('click', openMenu);
